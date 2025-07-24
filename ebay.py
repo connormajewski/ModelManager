18c418bd29e96b95b3e636538ac271775fa56e05
connormajewski <cmajews2@gmail.com>
2025-07-24 12:56:58 -0400

    Removed get_condition_id(), set_policy_ids(), and get_listing_categories(). These functions were used in testing API and are not needed in production program.

--- ebay.py
+++ ebay.py
@@ -117,85 +117,6 @@ def get_policy_ids():
             print(response.text)
             
     return policyIds
-
-def set_policy_ids():
-    
-    payment_endpoint = f"https://api.ebay.com/sell/account/v1/payment_policy"
-    shipping_endpoint = f"https://api.ebay.com/sell/account/v1/shipping_policy"
-    return_endpoint = f"https://api.ebay.com/sell/account/v1/return_policy"
-    
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
-    }
-    
-    payment_policy = {
-    
-        "name" : "Sandbox_Payment_Policy",
-        "marketplaceId" : "EBAY_US",
-        "categoryTypes" : [
-            {
-                "name" : "ALL_EXCLUDING_MOTORS_VEHICLES"
-            }
-        ],
-    
-        "immediatePay" : True
-    
-    }
-    
-    shipping_policy = {
-        "name": "Sandbox Shipping Policy",
-        "marketplaceId": "EBAY_US",
-        "categoryTypes": [
-            {
-                "name": "ALL_EXCLUDING_MOTORS_VEHICLES"
-            }
-        ],
-        "handlingTime": {
-            "unit": "DAY",
-            "value": 1
-        },
-        "shippingOptions": [
-            {
-                "optionType": "DOMESTIC",
-                "shippingServices": [
-                    {
-                        "shippingServiceCode": "USPSFirstClass",
-                        "sortOrderId": 1,
-                        "shippingCost": {
-                            "currency": "USD",
-                            "value": "0.00"
-                        }
-                    }
-                ]
-            }
-        ]
-    }
-    
-    return_policy = {
-        "name": "Sandbox Return Policy",
-        "marketplaceId": "EBAY_US",
-        "categoryTypes": [
-            {
-                "name": "ALL_EXCLUDING_MOTORS_VEHICLES"
-            }
-        ],
-        "returnsAccepted": True,
-        "returnPeriod": {
-            "value": 30,
-            "unit": "DAY"
-        },
-        "refundMethod": "MONEY_BACK",
-        "returnShippingCostPayer": "BUYER"
-    }
-    
-    payment_response = requests.post(payment_endpoint, headers=headers, data=json.dumps(payment_policy))
-    shipping_response = requests.post(shipping_endpoint, headers=headers, data=json.dumps(shipping_policy))
-    return_response = requests.post(return_endpoint, headers=headers, data=json.dumps(return_policy))
-    
-    print(f"{payment_response}\n{payment_response.text}\n\n")
-    print(f"{shipping_response}\n{shipping_response.text}\n\n")
-    print(f"{return_response}\n{return_response.text}\n\n")
    
 def refresh_access_token():
     
@@ -270,32 +191,6 @@ def create_inventory_body():
     }
 
     return None
- 
-def get_condition_id(categoryID):
-    
-    endpoint = f"https://api.ebay.com/sell/metadata/v1/marketplace/EBAY_US/get_item_condition_policies?category_ids=262320"
-    
-    headers = {
-    
-        "Authorization" : f"Bearer {access_token}",
-        "Content-Type" : "application/json",
-        "Content-Language" : "en-US"
-    
-    }
-    
-    inventory_response = requests.get(endpoint, headers=headers)
-    
-    if inventory_response.status_code == 200:
-        
-        print("INVENTORY SUCCESSFULLY.")
-        
-        inventory_data = inventory_response.json()
-        
-        print(inventory_data)
-        
-    else:
-        
-        print("INVENTORY FAILED.")
  
 def create_test_inventory_item(image_urls, model_obj):
     
@@ -400,32 +295,6 @@ def get_test_inventory_item(sku):
         
         print("INVENTORY FAILED.")
         
-def get_condition():
-    
-    endpoint = f"https://api.ebay.com/commerce/taxonomy/v1"
-    #endpoint = f"https://api.ebay.com/sell/inventory/v1/inventory_item/"
-    
-    headers = {
-    
-        "Authorization" : f"Bearer {access_token}",
-        "Content-Language" : "en-US"
-    
-    }
-    
-    inventory_response = requests.get(endpoint, headers=headers)
-    
-    if inventory_response.status_code == 200:
-        
-        print("INVENTORY SUCCESSFULLY.")
-        
-        inventory_data = inventory_response.json()
-        
-        print(inventory_data)
-        
-    else:
-        
-        print("INVENTORY FAILED.")
-
 def clear_inventory():
     
     skus = []
@@ -507,8 +376,6 @@ def create_test_listing(image_urls, model_obj):
     model_type = model_obj.model_type
     
     categoryId = categoryIds[model_type]
-    
-    get_condition_id(categoryId)
             
     print(f"{model_type} : {categoryId}")
     
@@ -607,19 +474,6 @@ def create_test_listing(image_urls, model_obj):
         
         return None
  
-def view_listing(item_id):
-        
-    url = f"https://api.ebay.com/buy/browse/v1/item/v1|{item_id}"
-
-    headers = {
-        "Authorization": f"Bearer {access_token}",  # Token must include buy.browse scope
-        "Content-Type": "application/json"
-    }
-
-    response = requests.get(url, headers=headers)
-    print(response.status_code)
-    print(response.json())
- 
 def check_offerId_exists(sku):
     
     #check_endpoint = f"https://api.ebay.com/sell/inventory/v1/offer?sku={sku}"
@@ -684,9 +538,6 @@ def set_location(merchant_location_key):
     print(response.status_code)
     print(response.text)
 
-# Uploads an image to EPS (Ebay image hosting) to allow it to be linked to an inventory/listing. Cannot create a listing through REST API if there is no image.
-# Had to use Trading XML, could not find a way to use REST API to do this.
-
 def upload_listing_photo(image_path, image_name):
     
     endpoint = "https://api.ebay.com/ws/api.dll"
@@ -738,58 +589,6 @@ def upload_listing_photo(image_path, image_name):
         
     else:
         print("ERROR UPLOADING.") 
- 
-def get_listing_categories():
-    
-        category_file = open("category_file.txt", "w", encoding="utf-8")
-
-        endpoint = "https://api.ebay.com/ws/api.dll"
-        
-        xml_payload = f"""<?xml version="1.0" encoding="utf-8"?>
-        <GetCategoriesRequest xmlns="urn:ebay:apis:eBLBaseComponents">
-          <RequesterCredentials>
-            <eBayAuthToken>{access_token}</eBayAuthToken>
-          </RequesterCredentials>
-          <DetailLevel>ReturnAll</DetailLevel>
-          <LevelLimit>0</LevelLimit>
-          <ViewAllNodes>true</ViewAllNodes>
-          <CategorySiteID>0</CategorySiteID>
-        </GetCategoriesRequest>
-        """
-        
-        headers = {
-            "X-EBAY-API-CALL-NAME": "GetCategories",
-            "X-EBAY-API-SITEID": "0",
-            "X-EBAY-API-COMPATIBILITY-LEVEL": "967",
-            "X-EBAY-API-DEV-NAME": dev_id,
-            "X-EBAY-API-APP-NAME": client_id,
-            "X-EBAY-API-CERT-NAME":client_secret,
-        }
-        
-        files = {
-            "XML Payload": (None, xml_payload, "text/xml"),
-        }
-
-        response = requests.post(endpoint, headers=headers, files=files)
-        
-        xml_content = response.text  # your XML response string
-
-        root = ET.fromstring(xml_content)
-
-        namespaces = {'ebay': 'urn:ebay:apis:eBLBaseComponents'}
-
-        # Use namespace prefix in find with the dictionary
-        category_array = root.find('.//ebay:CategoryArray', namespaces)
-        
-        if category_array is not None:
-            for category in category_array.findall('ebay:Category', namespaces):
-                cat_id = category.find('ebay:CategoryID', namespaces)
-                cat_name = category.find('ebay:CategoryName', namespaces)
-                category_file.write(f"{cat_name.text if cat_name is not None else '' : <64} : {cat_id.text if cat_id is not None else ''}\n")
-        else:
-            print("No categories found in response.")
-        
-        category_file.close()
         
 def get_listing_locations():
     
@@ -820,7 +619,6 @@ def get_listing_locations():
         
         print("ERROR FINDING LISTING LOCATIONS.")
           
-
 def check_access_token_validity():
 
     endpoint = f"https://api.ebay.com/sell/account/v1/payment_policy?marketplace_id=EBAY_US"
